6b0bd36b9030a2df96aec6c122998ceed10d9111
Raja Raghav <raja_raghav@hotmail.com>
2018-04-22 11:41:15 +0530

    redux settings added

---
+++ client/src/Component/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../Action";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
+  render() {
+    return (
+      <div>
+        <Router>
+          <div>
+            <div>
+              <Header />
+            </div>
+            <Route path="/" component={Dashboard} />
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+export default connect(null, actions)(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import App from "./Component/App";
+import Reducers from "./Reducer";
+import reduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(Reducers, {}, applyMiddleware(reduxThunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
+console.log(process.env.REACT_APP_STRIPE_KEY);
+console.log(process.env.NODE_ENV);
